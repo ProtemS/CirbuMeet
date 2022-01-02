08febf8615651c074df7f91440e53b7a921f7125
Protem <rotems456@gmail.com>
2022-01-03 00:41:11 +0200

    added an icon lol

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CustomHeader from '../components/CustomHeader';
 
 const Home = () => {
+	// useEffect(() => {
+	// 	fetch('http://localhost:8000/users').then((response) => {
+	// 		response.text().then(console.log);
+	// 	});
+	// }, []);
+	const [userName, setUserName] = useState('');
+	const [password, setPassword] = useState('');
 	return (
 		<>
-			<CustomHeader />
+			<CustomHeader title="CirbuMeet" />
+
 			<main className="min-h-screen flex flex-col items-center justify-center bg-gray-200">
-				<button className="bg-indigo-500 shadow-2xl shadow-indigo-500/50 px-12 py-8 rounded-2xl text-white text-7xl font-bold transition-all duration-200 hover:bg-indigo-500 hover:scale-105 active:scale-100 active:shadow-none hover:border-8 hover:border-white hover:shadow-gray-500/50">
-					Hell
-				</button>
+				<div className="flex flex-row items-center">
+					<span className="text-3xl">CirbuMeet</span> <img src="/icons8-blankie-48.png" alt="" />{' '}
+				</div>
+				<div>CirbuMeet is a website dedicated to chatting with other people</div>
+				<div className="mt-6">Login:</div>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						console.log(userName + password);
+					}}
+					className="flex flex-col space-y-2"
+				>
+					<input
+						type="text"
+						onChange={(e) => {
+							setUserName(e.target.value);
+						}}
+						placeholder="Username"
+					/>
+					<input
+						type="password"
+						onChange={(e) => {
+							setPassword(e.target.value);
+						}}
+						placeholder="Password"
+					/>
+
+					<button type="submit">Continue</button>
+				</form>
 			</main>
 		</>
 	);
