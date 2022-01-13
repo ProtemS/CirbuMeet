6eca61300797726550279efc69d36189572a324f
Sarah Smith <rotems456@gmail.com>
2022-01-14 00:51:56 +0200

    made login page look

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,63 @@
 import React, { useState } from 'react';
 
+import { LockClosedIcon, UserIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
 import CustomHeader from '../components/CustomHeader';
+import InputLabel from '../components/InputLabel';
 
 const Home = () => {
-	// useEffect(() => {
-	// 	fetch('http://localhost:8000/users').then((response) => {
-	// 		response.text().then(console.log);
-	// 	});
-	// }, []);
 	const [userName, setUserName] = useState('');
 	const [password, setPassword] = useState('');
 	return (
 		<>
 			<CustomHeader />
 
-			<main className="min-h-screen flex flex-col items-center justify-center bg-indigo-50">
-				<div className="flex flex-row items-center select-none ">
-					<span className="text-3xl font-semibold">CirbuMeet</span>
-					<img src="/icons8-blankie-48.png" alt="" />
-				</div>
-				<div>CirbuMeet is a website dedicated to chatting with other people</div>
-				<div className="mt-6">Login:</div>
+			<main className="min-h-screen flex flex-col items-center justify-center bg-indigo-300">
 				<form
 					onSubmit={(e) => {
 						e.preventDefault();
 						console.log(userName + password);
 					}}
-					className="flex flex-col space-y-2"
+					className="flex flex-col items-center m-4 w-full sm:flex-none sm:w-full sm:max-w-md min-w-max bg-white bg-opacity-80 backdrop-filter backdrop-blur-lg py-8 px-4 sm:px-8 rounded-lg shadow-lg border-2 border-white overflow-auto"
 				>
-					<input
-						type="text"
-						onChange={(e) => {
-							setUserName(e.target.value);
-						}}
-						placeholder="Username"
-						className="border-2 border-gray-700 outline-none w-72"
+					<div className="flex justify-center items-center text-6xl font-semibold">
+						<span>CirbuMeet</span>
+						<img src="/icons8-blankie-48.png" alt="" className="w-14" />
+					</div>
+					<div className="text-gray-900 pt-1 text-3xl">Start messaging with CirbuMeet</div>
+					<InputLabel
+						placeholder="Enter  username"
+						icon={UserIcon}
+						value={userName}
+						setValue={setUserName}
+						text="Username"
+						className="w-full pt-5"
 					/>
-					<input
-						type="password"
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}
-						placeholder="Password"
-						className="border-2 border-gray-700 outline-none"
+					<InputLabel
+						placeholder="Enter password"
+						icon={LockClosedIcon}
+						value={password}
+						setValue={setPassword}
+						text="Password"
+						password
+						className="w-full pt-5"
 					/>
-
-					<button type="submit">Continue</button>
+					<button
+						type="submit"
+						className="w-full p-4 mt-6 rounded-lg bg-sky-900 text-gray-300 hover:text-white transition-all ease-out font-bold"
+					>
+						Log in
+					</button>
+					<Link href="/register">
+						<a className="no-underline mt-14 p-4 w-full flex items-center justify-center rounded-lg  bg-sky-900">
+							<div className="no-underline text-gray-300 hover:text-white transition-all ease-out font-bold">
+								{/*  eslint-disable-next-line react/no-unescaped-entities */}
+								Don't have a user? Register!
+							</div>
+						</a>
+					</Link>
 				</form>
-				<Link href="/register">
-					<a>Dont have an account? Register!</a>
-				</Link>
 			</main>
 		</>
 	);
