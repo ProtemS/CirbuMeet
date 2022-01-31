af17df4278e26af62e0d15f259d13d4a52f62be3
Protem <rotems456@gmail.com>
2022-01-31 07:57:56 +0200

    e

---
+++ src/components/ChatMessage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ChatMessage = () => {
+	return (
+		<div className="flex flex-row bg-white rounded-lg p-3 m-1">
+			<div className="font-semibold">Username - </div>
+			<div className="ml-2 flex flex-wrap"> Messageeeeeeeeeeee</div>
+		</div>
+	);
+};
--- src/pages/chatpage.js
+++ src/pages/chatpage.js
@@ -1,14 +1,18 @@
 /* eslint-disable unused-imports/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 
+import { PencilIcon } from '@heroicons/react/outline';
 import axios from 'axios';
 
+import { ChatMessage } from '../components/ChatMessage';
 import CustomHeader from '../components/CustomHeader';
+import InputLabel from '../components/InputLabel';
 import useLocalStorage from '../components/useLocalStorage';
 
 const Chatpage = ({ ...props }) => {
 	const [token, setToken] = useLocalStorage('token');
 	const [valid, setValid] = useState(false);
+	const [sendMessage, setSendMessage] = useState(null);
 	useEffect(() => {
 		console.log(token);
 		const getLogged = async () => {
@@ -27,7 +31,27 @@ const Chatpage = ({ ...props }) => {
 		<>
 			<CustomHeader title="CirbuMeet Chatpage" />
 
-			{valid ? <div>ur good man</div> : <div>Ur smoked fam</div>}
+			{valid ? (
+				<div className="min-h-screen bg-radial-at-t from-sky-400 to-indigo-900 flex justify-center items-center">
+					<div className="m-4 w-full sm:flex-none sm:w-full sm:max-w-md min-w-max bg-white bg-opacity-80 backdrop-filter backdrop-blur-lg py-16 px-10 sm:px-8 rounded-lg shadow-lg border-2 border-white">
+						<div className="font-semibold  text-xl">Logged in as (shit)</div>
+						<div className="overflow-y-auto max-h-96">
+							<ChatMessage />
+							<ChatMessage />
+							<ChatMessage />
+							<ChatMessage />
+						</div>
+						<InputLabel
+							value={sendMessage}
+							setValue={setSendMessage}
+							icon={PencilIcon}
+							placeholder="Send a message"
+						/>
+					</div>
+				</div>
+			) : (
+				<div>Ur smoked fam</div>
+			)}
 		</>
 	);
 };
